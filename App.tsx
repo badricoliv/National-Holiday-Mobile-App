4501a51f5132e9d56b8c3200184738c5df0eeca8
Ollie Badrick <49539730+badricoliv@users.noreply.github.com>
2021-09-01 13:23:16 +1200

    connected to firebase
    displays the holidays on that day (if there are any in the database)

--- App.tsx
+++ App.tsx
@@ -1,16 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {db} from './firebase'
 
+class holiday {
+  day;
+  month;
+  name;
+  constructor(day:number, month:number,name:string) {
+    this.day = day;
+    this.month = month;
+    this.name = name;
+  }
+}
 function App() {
   var date = new Date().toDateString();
   var month = new Date().getMonth();
   var day = new Date().getDate();
+  const coll = db.firestore().collection('Holidays')
+  const ref = db.firestore().collection('Holidays').where("day","==",day);
+  const ref2 = ref.where("month","==",month);
+  const [data,setData] = useState("");
+  var strings: string[] = [];
+  /* Code to add data to firestore */
+  const days = [
+  new holiday(1,8, "American Chess Day"),
+  new holiday(1,8, "Building and Code Staff Appreciation Day"),
+  new holiday(1,8, "Chicken Boy Day"),
+  new holiday(1,8,"National No Rhyme Nor Reason Day"),
+  new holiday(1,8,"Wattle Day"),
+  new holiday(1,8,"World Letter Writing Day"),
+]
+for(var i = 0; i < 6; i++) {
+  const user = coll.doc(days[i].name).set({
+    day: days[i].day,
+    month: days[i].month,
+  })
+}
+
+  useEffect(() => {
+    async function getDataFromFireStore() {
+        ref.onSnapshot((query) => {
+            const objs : string[] = [];
+            query.forEach((doc) => {
+                objs.push(doc.id);
+            });
+            let str : string = "";
+            objs.forEach(function (value) {
+                str = str + value + "\n"
+            });
+            setData(str);
+            strings = objs;
+        });
+    }
+
+    getDataFromFireStore();
+  }, []);
+  
+  var month = new Date().getMonth();
+  var day = new Date().getDate();
   return (
     <View style={styles.container}>
-      <Text>{day}</Text>
-      <Text>{month}</Text>
-      <Text>{date}</Text>
+      <Text>Today, {date}, is:</Text>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );
